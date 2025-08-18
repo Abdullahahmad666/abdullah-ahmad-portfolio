5e5732a91bba0b7a4717656170f7e9410617d42d
Abdullah <l201326@lhr.nu.edu.pk>
2025-08-18 22:21:29 +0500

    Correctly Installed Vite Packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,12 +10,15 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "hls.js": "hls.js/dist/hls.js", // ✅ use non-minified version // Ensure hls.js is resolved correctly
     },
+  },
+   esbuild: {
+    sourcemap: false, // ✅ stop loading broken .map files
   },
 }));
